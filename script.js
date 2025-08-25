982f0d9a7f5c84fcc86d0447b1265325d61ccc1a
shankar kumar ray <rays76855@gmail.com>
2025-08-25 18:18:37 +0530

    Initial commit for smartCart

---
+++ script.js
@@ -0,0 +1,28 @@
+// const bar =document.getElementById('bar');
+// const nav = document.getElementById('navbar');
+// if(bar){
+//     bar.addEventListener('click', () =>{
+//         nav.classList.add('active');
+//     })
+// }
+// const x = document.getElementById('navbar');
+// function fun1(){
+//     x.style.right = "0px";
+// }
+var x = 0;
+function fun1(){
+    var bar = document.getElementById('bar');
+    var right = document.getElementById('navbar');
+    if(x==0){
+        right.style.right = "-100px";
+        bar.style.backgroundColor="rgba(255, 255, 255, 1)";
+        bar.style.boxShadow="0px 0px 5px 5px rgba(143, 143, 143, 0.51)";
+        x=1;
+    }
+    else{
+        right.style.right = "-500px";
+        bar.style.backgroundColor="rgba(255, 255, 255, 0)";
+        bar.style.boxShadow="0px 0px 5px 5px rgba(143, 143, 143, 0)";
+        x=0;
+    }
+}
